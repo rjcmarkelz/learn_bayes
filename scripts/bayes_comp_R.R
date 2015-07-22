b8c819c5fd021fc1ef39c70d071f383381020c2b
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-07-22 14:03:47 -0400

    chapter 2

--- scripts/bayes_comp_R.R
+++ scripts/bayes_comp_R.R
@@ -108,6 +108,96 @@ binout
 
 # chapter 2
 
+# discrete priors over a grid
+p = seq(0.05, 0.95, by = 0.1)
+prior = c(1, 5.2, 8, 7.2, 4.6, 2.1, 0.7, 0.1, 0, 0)
+prior = prior/sum(prior)
+
+plot(p, prior, type = "h", ylab= "prior prob")
+
+data = c(11,16)
+post = pdisc(p, prior, data)
+round(cbind(p, prior, post), 2)
+
+library(lattice)
+PRIOR = data.frame("prior", p, prior)
+POST  = data.frame("posterior", p , post)
+names(PRIOR) = c("Type", "p", "prob")
+names(POST) = c("Type", "p", "prob")
+data = rbind(PRIOR, POST)
+
+xyplot(prob~p|Type, data = data, layout = c(1,2),
+	type = "h", lwd = 3, col = "black")
+
+a = 3.26; b = 7.19; s = 11; f = 16;
+curve(dbeta(x, a+s, b+f), from = 0, to = 1, xlab = "p", ylab = "Density", lty = 1, lwd = 4)
+curve(dbeta(x, s+1, f+1), add = TRUE, lty = 2, lwd = 4)
+curve(dbeta(x, a, b), add = TRUE, lty = 3, lwd = 4)
+
+#calculate the posterior that the proportion of heavy sleepers is greater than 0.5?
+1 - pbeta(0.5, a + s, b + f)
+# this probability is small
+
+# calculate the 95% CI, this is exact
+qbeta(c(0.05, 0.95), a + s, b + f)
+
+# or we could do simulations to get the calculated values
+ps = rbeta(1000, a + s, b + f)
+hist(ps)
+sum(ps >= 0.5)/1000
+# calculate the 90 interval from the simulated samples
+quantile(ps, c(0.05, 0.95))
+
+# use the histprior function in learnbayes
+# create a grid and then calculate the posterior over the grid
+midpt = seq(0.05, 0.95, by = 0.1) # set up the grid
+prior = c(1, 5.2, 8, 7.2, 4.6, 2.1, 0.7, 0.1, 0, 0) # prior weights on the grid
+prior = prior/sum(prior)
+prior
+
+curve(histprior(x, midpt, prior), from = 0, to = 1, ylab = "Prior density", ylim = c(0, 0.3))
+# take a look at the blocky grid
+
+curve(histprior(x, midpt, prior)*dbeta(x, s + 1, f + 1), from = 0, to = 1, ylab = "post density")
+p = seq(0, 1, length = 500)
+p
+post = histprior(p, midpt, prior)*dbeta(p, s+1, f+1)
+post = post/sum(post)
+
+# sample with replacement 
+ps = sample(p, replace = TRUE, prob = post)
+hist(ps)
+
+# output vector
+p = seq(0.05, 0.95, by = .1)
+prior = c(1, 5.2, 8, 7.2, 4.6, 2.1, 0.7, 0.1, 0, 0 )
+prior = prior/sum(prior)
+m = 20; ys = 0:20
+pred = pdiscp(p, prior, m , ys)
+round(cbind(0:20, pred), 3)
+
+# beta prior
+ab = c(3.26, 7.19)
+m = 20; ys = 0:20
+pred = pbetap(ab, m, ys)
+
+#simulate draws from the prior and store them
+p = rbeta(1000, 3.26, 7.19)
+y = rbinom(1000, 20, p)
+freq = table(y) # save the frequencies out of 1000 draws on y
+ys = as.integer(names(freq))
+predprob = freq/sum(freq)
+
+plot(ys, predprob, type = "h", xlab = "y")
+dist = cbind(ys, predprob)
+dist 
+
+covprob = 0.9
+discint(dist, covprob)
+
+
+
+
 
 
 
