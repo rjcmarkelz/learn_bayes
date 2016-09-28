c7d3a704156dd90f658d9b6f8e0f4df025f43e96
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-09-28 10:07:09 -0700

    learning by inferences

--- scripts/graph-models.R
+++ scripts/graph-models.R
@@ -149,3 +149,56 @@ jtree3 <- setEvidence(jtree, evidence = list(F = "true", A = "false"))
 querygrain(jtree, nodes = c("C"), type = "marginal")
 querygrain(jtree2, nodes = c("C"), type = "marginal")
 querygrain(jtree3, nodes = c("C"), type = "marginal")
+
+# Chapter 3- Learning parameters from the data
+library(plyr)
+iris <- read.csv("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data",col.names=c("sepal_length","sepal_width","petal_length","petal_width","class"))
+iris
+
+iris_dist <- daply(iris, .(class), nrow) / nrow(iris)
+iris_dist
+
+# descretize the dataset
+head(iris)
+q <- quantile(iris$sepal_width,seq(0,1,.33))
+iris$dsw[iris$sepal_width < q['33%']] <- "small"
+iris$dsw[iris$sepal_width >= q['33%'] & iris$sepal_width < q['66%']] <- "medium"
+iris$dsw[iris$sepal_width > q['66%']] <- "large"
+
+
+p1 <- daply(iris,.(dsw,class), function(n) nrow(n))
+p1 <- p1 / colSums(p1)
+
+# bayesian learning example
+# prob is vector of probability for each Theta
+# nh and nt are number heads and number tails
+# Theta is the vector of possible values of for Theta
+posterior <- function(prob, nh, nt, Theta = c(0.2, 0.5, 0.8)){
+     x = numeric(3)
+     for(i in 1:3){
+        x[i] = prob[i]* (Theta[i]^nh)*((1 - Theta[i])^nt)
+     }
+     norm = sum(x)
+     return(x/norm)
+}
+
+posterior(c(0.2, 0.75, 0.05), 2, 8)
+posterior(c(0.2,0.75,0.05),50,50)
+#outputs a distribution summing to 1! Machine limitation in error rate
+# therefore do LOG values
+
+posterior2 <- function(prob, nh, nt, Theta = c(0.2, 0.5, 0.8)){
+     x = numeric(3)
+     for(i in 1:3){
+        x[i] = exp(log(prob[i]) + nh*log((Theta[i])) + nt*log(1 - Theta[i]))
+     }
+     norm = sum(x)
+     return(x/norm)
+}
+posterior2(c(0.2,0.75,0.05),50,50) #does not appear to change output
+
+#prior distribution is uniform
+posterior(c(1/3,1/3,1/3),2,8,c(0.2,0.5,0.8))
+posterior(c(1/3,1/3,1/3),8,2,c(0.2,0.5,0.8))
+posterior2(c(1/3,1/3,1/3),5,5,c(0.2,0.5,0.8))
+
