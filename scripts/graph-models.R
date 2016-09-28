da3e909cf6a23d1242b6566f813be39bfa58b56a
Cody Markelz <rjmarkelz@ucdavis.edu>
2016-09-28 13:39:26 -0700

    finish ML

--- scripts/graph-models.R
+++ scripts/graph-models.R
@@ -203,6 +203,83 @@ posterior(c(1/3,1/3,1/3),8,2,c(0.2,0.5,0.8))
 posterior2(c(1/3,1/3,1/3),5,5,c(0.2,0.5,0.8))
 
 # maximum likelihood
+library(graph)
+library(Rgraphviz)
+library(plyr)
 
+data0 <- data.frame(
+       x = c("a","a","a","a","b","b","b","b"),
+       y = c("t","t","u","u","t","t","u","u"),
+       z = c("c","d","c","d","c","d","c","d"))
+data0
+
+edges0 <- list(x = list(edges = 2), y = list(edges = 3), z = list())
+edges0
+
+g0 <- graphNEL(nodes = names(data0), edgeL = edges0, edgemod = "directed")
+plot(g0)
+
+data1 <- read.csv("http://archive.ics.uci.edu/ml/machine-learning-databases/nursery/nursery.data", col.names=c("parents","has_nurs","form","children","housing","finance","social","health","class"))
+head(data1)
+
+edges1 <- list(parents = list(), has_nurs = list(), form = list(), children = list(),
+                housing = list(), finance = list(), social = list(), health = list(),
+                class = list(edges = 1:8))
+
+g1 <- graphNEL(nodes = names(data1), edgeL = edges1, edgemod = "directed")
+plot(g1)
+
+# computes frequency of one combination of variable of interest
+make_cpt <- function(df, pa){
+    prob <- nrow(df)
+    parents <- data.frame(df[1,pa])
+    names(parents) <- pa
+
+    data.frame(parents, prob)
+}
+
+# takes two paramters, the graph and the data
+# To find the parents of all the variables it reverses the edge direction
+learn <- function(g, data){
+    rg <- reverseEdgeDirections(g)
+    result <- list()
+
+    #now look at each variable independently
+    for(var in rg@nodes){
+        pa <- unlist(adj(rg,var)) # find parents of each node
+        # if node has parent then conditional probability
+        if(length(pa) > 0){
+            # for each possible value and its parents, calculate frequency
+            X <- ddply(data, c(var,pa), make_cpt, pa)
+            Y <- ddply(data, pa, make_cpt, pa)
+
+            # apply bayes formula to transform counts to probabilities
+            for(i in 1:nrow(Y)){
+                c <- sapply(1:nrow(X), function(j) all(X[j,pa] == Y[i,pa]))
+                c <- which(c)
+                X$prob[c] <- X$prob[c]/Y$prob[i]
+            }
+        }
+        # node does not have a parent, calculate marginal
+        # apply bayes formula to transform marginal counts to probabilities (frequencies)
+        else {
+            X <- ddply(data, var, function(df) c(prob = nrow(df)))
+            X$prob <- X$prob/sum(X$prob)
+        }
+
+        result[[length(result) + 1]] <- X
+    }
+
+    return(result)
+}
+
+# now for some learning
+# test on small dataset
+learn(g0, data0) 
+
+learn(g1, data1)
+# notice that some of the variables have prob = 1!
+# this is because there is only 1 observation of that in the dataset!
+# this is where hyper parameters come into play to bound prob between 0 and 1
 
 
